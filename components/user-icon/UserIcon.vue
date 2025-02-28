f2a333468727f7bcd8c0b2834881171a89352989
sebastian Högsander <seblhog@gmail.com>
2025-02-28 02:16:50 +0100

    chore: add responsive login cue on desktop view

--- components/user-icon/UserIcon.vue
+++ components/user-icon/UserIcon.vue
@@ -1,9 +1,12 @@
 <template>
 	<section class="user-icon-wrapper">
 		<img src="~/assets/site_icons/user-icon.svg" />
+		<!-- Make P tag into a link to /login -->
+		<p class="user-icon-login-cue">{{ coreData.userIconLoginCue }}</p>
 	</section>
 </template>
 
 <script lang="ts" setup>
 	import './style.scss';
+	import coreData from '~/data/core.json';
 </script>
