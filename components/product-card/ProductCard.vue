fa3cb8fc0e9e4503387d3a4b4229583d41b8affd
sebastian Högsander <seblhog@gmail.com>
2025-04-15 23:49:32 +0200

    refactor: restarting product-card component

--- components/product-card/ProductCard.vue
+++ components/product-card/ProductCard.vue
@@ -1,18 +1,5 @@
 <template>
-	<site-button class="product-card" :to="`/recipe/${product._id}`">
-		<img :src="product.thumbnail" :alt="'image of' + product.title" class="product-thumbnail">
-		<div class="card-body">
-			<h1 class="card-title">{{ product.title }}</h1>
-			<p class="card-description">{{ product.description }}</p>
-			<div class="card-footer">
-				<div class="card-read-more" :to="`/recipe/${product._id}`">
-					Read more
-				</div>
-				<user-icon :user="props.product.creator" />
-			</div>
-		</div>
-
-	</site-button>
+	<div>{{ product.title }}</div>
 </template>
 
 <script lang="ts" setup>
@@ -28,5 +15,5 @@ interface ProductCardProps {
 	date_posted: Date;
 }
 
-const props = defineProps<{ product: ProductCardProps }>();
+defineProps<{ product: ProductCardProps }>();
 </script>
