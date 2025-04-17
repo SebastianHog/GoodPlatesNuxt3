4ec61cf1ab0508c4ecd7a1f2bda80c15c793b580
sebastian Högsander <seblhog@gmail.com>
2025-04-18 00:05:13 +0200

    feat: mobile product card

--- components/product-card/ProductCard.vue
+++ components/product-card/ProductCard.vue
@@ -1,7 +1,15 @@
 <template>
-	<section class="product-card-wrapper">
-
-	</section>
+	<site-button :to="`/recipe/${product._id}`" class="product-card-wrapper">
+		<img :src="product.thumbnail" alt="recipe thumbnail" class="card-thumbnail" />
+		<div class="card-body">
+			<h2 class="card-title">{{ product.title }}</h2>
+			<p class="card-creator">By <site-button class="card-profile-link" :to="`/user/${product.creator._id}`"
+					variant="transparent"> {{
+						product.creator.username }} </site-button>
+			</p>
+			<p class="card-description">{{ firstSentence }}</p>
+		</div>
+	</site-button>
 </template>
 
 <script lang="ts" setup>
@@ -12,10 +20,19 @@ interface ProductCardProps {
 	_id: string;
 	title: string;
 	description?: string;
-	thumbnail?: string;
+	thumbnail: string;
 	creator: IUser;
 	date_posted: Date;
 }
 
-defineProps<{ product: ProductCardProps }>();
+
+const props = defineProps<{ product: ProductCardProps }>();
+
+const firstSentence = computed(() => {
+	if (!props.product.description) return '';
+
+	const match = props.product.description.match(/^.*?[.!?](?:\s|$)/);
+	return match ? match[0].trim() : props.product.description;
+});
+
 </script>
