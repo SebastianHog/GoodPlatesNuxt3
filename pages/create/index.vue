aea61de158ab954ce1719bb95f73bc32b6dfa50c
sebastian Högsander <seblhog@gmail.com>
2025-04-25 00:01:26 +0200

    starting recipe page

--- pages/create/index.vue
+++ pages/create/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    Create recipe page
-  </div>
+  <section class="create-recipe-page-wrapper">
+    <div class="add-image-wrapper">
+      <div>
+        <input type="text" :placeholder="imagUrlPlaceholder" class="add-image-input" v-model="imageUrl">
+        <site-button @click="appendNewImage" class="add-image-action">Add image</site-button>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script lang="ts" setup>
+import './style.scss';
+import core from '~/data/core.json';
+
+const imageUrl = ref('');
+
+const imagUrlPlaceholder = core.pages.createRecipe.imageUrlPlaceholder;
+
+const appendNewImage = () => {
+  console.log(imageUrl.value);
+}
+</script>
