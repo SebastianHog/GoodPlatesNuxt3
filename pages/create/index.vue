d830417512066d4bb9c3ef365e588f51222636a3
sebastian Högsander <seblhog@gmail.com>
2025-04-25 23:11:39 +0200

    feat: first version of requirement adding

--- pages/create/index.vue
+++ pages/create/index.vue
@@ -7,13 +7,40 @@
       </div>
       <div v-else>
         <input type="text" :placeholder="imagUrlPlaceholder" class="add-image-input" v-model="imageUrl" />
-        <site-button @click="appendImage" class="add-image-action">Add image</site-button>
+        <site-button @click="appendImage" class="add-image-action">{{ core.pages.createRecipe.buttons.addImage
+        }}</site-button>
       </div>
     </div>
+
     <div class="create-recipe-form-wrapper">
       <input type="text" :placeholder="recipeTitlePlaceholder" class="title-input" v-model="recipeTitle" />
-      <textarea v-model="recipeDescription" class="description-input"></textarea>
+      <textarea :placeholder="recipeDescriptionPlaceholder" v-model="recipeDescription"
+        class="description-input"></textarea>
     </div>
+
+    <div class="recipe-req-instructions">
+      <div class="recipe-requirements">
+        <h1 class="input-box-title">Requirements</h1>
+        <div class="requirements-inner">
+          <div class="item-input-wrapper">
+            <div class="item-input-header">
+              <h2 class="item-label">Item</h2>
+              <h2 class="amount-label">Amount</h2>
+              <h2 class="unit-label">Unit</h2>
+            </div>
+
+            <div class="input-requirements">
+              <div class="item" v-for="(req, idx) in requirements" :key="idx">
+                <input type="text" class="item-name" placeholder="Item" v-model="req.name" />
+                <input type="text" class="item-amount" placeholder="Amount" v-model="req.amount" />
+                <input type="text" class="item-unit" maxlength="10" placeholder="Unit" v-model="req.unit" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </section>
 </template>
 
@@ -29,8 +56,9 @@ let displayImage = ref(false);
 
 const validImageEndings = ['jpg', 'jpeg', 'png', 'webp', 'avif', 'bmp'];
 
-const imagUrlPlaceholder = core.pages.createRecipe.imageUrlPlaceholder;
-const recipeTitlePlaceholder = core.pages.createRecipe.recipeTitlePlaceholder;
+const imagUrlPlaceholder = core.pages.createRecipe.placeholderText.imageUrlPlaceholder;
+const recipeTitlePlaceholder = core.pages.createRecipe.placeholderText.recipeTitlePlaceholder;
+const recipeDescriptionPlaceholder = core.pages.createRecipe.placeholderText.recipeDescriptionPlaceholder;
 
 const appendImage = () => {
   if (!imageUrl.value || !validImageEndings.some((ending) => imageUrl.value.toLowerCase().endsWith(ending))) return;
@@ -41,5 +69,38 @@ const removeImage = () => {
   displayImage.value = false;
   imageUrl.value = '';
 }
+
+interface Requirement {
+  name: string
+  amount: string
+  unit: string
+}
+
+const requirements = ref<Requirement[]>([
+  { name: '', amount: '', unit: '' }
+])
+
+watch(
+  requirements,
+  () => {
+    while (requirements.value.length > 1) {
+      const last = requirements.value.at(-1)!
+      const prev = requirements.value.at(-2)!
+      const lastEmpty = !last.name && !last.amount && !last.unit
+      const prevEmpty = !prev.name && !prev.amount && !prev.unit
+      if (lastEmpty && prevEmpty) {
+        requirements.value.pop()
+      } else {
+        break
+      }
+    }
+
+    const final = requirements.value.at(-1)!
+    if (final.name || final.amount || final.unit) {
+      requirements.value.push({ name: '', amount: '', unit: '' })
+    }
+  },
+  { deep: true }
+)
 
 </script>
