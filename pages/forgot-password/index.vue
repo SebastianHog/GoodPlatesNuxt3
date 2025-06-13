61aa4242f71371f7e436927c3142b240f7e8da1d
Sebastian <seblhog@gmail.com>
2025-06-13 23:19:12 +0200

    feat: Forgot password page

---
+++ pages/forgot-password/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="dummy-pw-text">Should've written it down</h1>
+</template>
+
+
+<style lang="scss">
+.dummy-pw-text {
+  font-size: 3rem;
+  text-align: center;
+}
+</style>
