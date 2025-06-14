ff4a27ad53e06c3bfc5a5526bfac8ee52b045935
Sebastian <seblhog@gmail.com>
2025-06-14 03:13:49 +0200

    feat: My recipes page.

--- pages/my_recipes/index.vue
+++ pages/my_recipes/index.vue
@@ -1,27 +1,27 @@
 <template>
-  <section v-if="user">
-    <ProductCard v-for="recipe in userRecs.recipes" :product="recipe" :key="recipe._id" />
+  <section v-if="user && userRecs && userRecs.recipes" class="my-recipes-wrapper">
+    <ProductCard v-if="userRecs.recipes && userRecs.recipes" v-for="recipe in userRecs.recipes" :key="recipe._id"
+      :product="recipe" />
   </section>
   <section v-else>
-    <h1>Loading user...</h1>
+    <h1>Loading...</h1>
   </section>
 </template>
 
 <script lang="ts" setup>
-const route = useRoute();
+import './style.scss'
 import { useUserStore } from '../../stores/userStore';
-import type { IRecipe } from '~/types/recipe';
 import { useRecipeStore } from '~/stores/recipeStore';
 import type { IUser } from '~/types/user';
+
 
 const userStore = useUserStore();
 const user: IUser | null = userStore.user;
 
 const recipeStore = useRecipeStore();
 
-const { data: userRecs } = await useAsyncData<IRecipe[] | any>('recipe', async () => {
-  console.log(user._id)
-  if (!user?._id) return;
+const { data: userRecs } = await useAsyncData('recipe', async () => {
+  if (!user || !user._id) return { recipes: [] };
   return await recipeStore.getRecipeByUser(user._id);
 });
 
